24fb2e072571e2650300246bd336290134c7528b
Nick Palenchar <nickpal@nickpalenchar.com>
2023-07-08 09:49:05 -0400

    initial

---
+++ report.py
@@ -0,0 +1,12 @@
+"""
+Reports on spending of each category and total leftover.
+The leftover can go into savings or some other big goal!
+"""
+from budgets.config import config
+
+def main(transactions_file, ):
+    pass
+
+
+if __name__ == '__main__':
+    print(config)
